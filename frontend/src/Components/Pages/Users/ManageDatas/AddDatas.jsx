1c7adb181e0a6130c827c80757007ba982535410
Sylvain-Z <sylvain.zoogones@gmail.com>
2025-01-13 18:17:27 +0100

    feat(addDatas) : responsive style

--- frontend/src/Components/Pages/Users/ManageDatas/AddDatas.jsx
+++ frontend/src/Components/Pages/Users/ManageDatas/AddDatas.jsx
@@ -31,30 +31,36 @@ export default function AddDatas() {
 
   return (
     <form onSubmit={handleSubmit} className="add-datas-form">
-      <label htmlFor="input1">Donnée1</label>
-      <input
-        placeholder="Entrez une donnée"
-        type="text"
-        name="input1"
-        value={input1}
-        onChange={(e) => setInput1(e.target.value)}
-      />
-      <label htmlFor="input2">Donnée2</label>
-      <input
-        placeholder="Entrez une donnée"
-        type="text"
-        name="input2"
-        value={input2}
-        onChange={(e) => setInput2(e.target.value)}
-      />
-      <label htmlFor="input3">Donnée3</label>
-      <input
-        placeholder="Entrez une donnée"
-        type="text"
-        name="input3"
-        value={input3}
-        onChange={(e) => setInput3(e.target.value)}
-      />
+      <div className="form-group">
+        <label htmlFor="input1">Donnée1</label>
+        <input
+          placeholder="Entrez une donnée"
+          type="text"
+          name="input1"
+          value={input1}
+          onChange={(e) => setInput1(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="input2">Donnée2</label>
+        <input
+          placeholder="Entrez une donnée"
+          type="text"
+          name="input2"
+          value={input2}
+          onChange={(e) => setInput2(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="input3">Donnée3</label>
+        <input
+          placeholder="Entrez une donnée"
+          type="text"
+          name="input3"
+          value={input3}
+          onChange={(e) => setInput3(e.target.value)}
+        />
+      </div>
       <button type="submit">
                 <FontAwesomeIcon icon={faCirclePlus} size="xl" /><p>Ajouter</p></button>
       {loading && <Loader />}
