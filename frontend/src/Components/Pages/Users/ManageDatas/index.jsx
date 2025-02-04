38a24002cdcff25fbfe760d0d7548002f540fb8f
Sylvain Zoogones <140527091+Sylvain-Z@users.noreply.github.com>
2025-02-04 13:06:30 +0100

    feat(dataManagement) : All data table

--- frontend/src/Components/Pages/Users/ManageDatas/index.jsx
+++ frontend/src/Components/Pages/Users/ManageDatas/index.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import DataTable from "./DatasTable";
 
+import mergedData from "./merged_data.csv";
 import AddDatas from "./AddDatas";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,10 +9,24 @@ import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function ManageDatas() {
   const [addDatasOpen, setAddDatasOpen] = useState(false);
+  const [allData, setAllData] = useState([]);
 
   const toggleAddDatas = () => {
     setAddDatasOpen((prevAddDatasOpen) => !prevAddDatasOpen);
   };
+
+  useEffect(() => {
+    fetch(mergedData)
+      .then((response) => response.text())
+      .then((text) => {
+        const rows = text.split("\n").slice(1);
+        const data = rows.map((row) => {
+          const [Country, Date, Population, Cases, Active, Recovered, Deaths] = row.split(",");
+          return { Country, Date, Population, Cases, Active, Recovered, Deaths };
+        });
+        setAllData(data);
+      });
+  }, []);
 
   return (
     <>
@@ -27,10 +43,12 @@ export default function ManageDatas() {
           {addDatasOpen && <AddDatas />}
         </article>
 
-        <article>
-          <h3>Tableau de données à venir</h3>
+        <article className="allDatas-article">
+          <h3>Toutes les donnés</h3>
+          <DataTable data={allData} />
         </article>
       </section>
+      <section className="section-end laptopAndDesktop-hidden"></section>
     </>
   );
 }
